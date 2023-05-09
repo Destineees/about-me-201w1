7079d1528837bf078fd43417a2b4340d47342a84
Destinee Sanders <destineees@gmail.com>
2023-05-09 02:05:14 -0700

    lab 3 initial

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ let youserName = prompt('Hello I am Destinee, What is your Name?');
 
 alert('Welcome to the About Destinee Guessing Game... Let the games begin ' + youserName);
 
-
+let youserScore = 0;
 // Prompt user with 5 yes/no questions and alert incorrect or correct
 
 let firstAnswer = prompt('Was I born in Seattle? (Y or N)').toUpperCase();
 
 if(firstAnswer === 'N' )
 {
+  youserScore++;
   alert('Correct! Great Job ' + youserName);
 }
 else
@@ -22,6 +23,7 @@ let secondAnswer = prompt('Did I work as a Flight attendant? (Y or N)').toUpperC
 
 if(secondAnswer === 'Y' )
 {
+  youserScore++;
   alert('Correct! Great Job ' + youserName);
 }
 else
@@ -31,6 +33,7 @@ let thirdAnswer = prompt('Do I have a real estate license? (Y or N)').toUpperCas
 
 if(thirdAnswer === 'Y' )
 {
+  youserScore++;
   alert('Correct! Great Job ' + youserName);
 }
 else
@@ -40,6 +43,7 @@ let fourthAnswer = prompt('Do I own property? (Y or N)').toUpperCase();
 
 if(fourthAnswer === 'N' )
 {
+  youserScore++;
   alert('Correct! Great Job ' + youserName);
 }
 else
@@ -49,11 +53,60 @@ let fifthAnswer = prompt('Did I study in Arizona? (Y or N)').toUpperCase();
 
 if(fifthAnswer === 'Y' )
 {
+  youserScore++;
   alert('Correct! Great Job ' + youserName);
 }
 else
   alert('Incorrect =[ ');
 
+let sixthAnswer = prompt('Guess a numer between 1-10. You have four times to guess.');
+let i = 0;
+do{
+  if (sixthAnswer < 5)
+  {
+    alert('Too low!');
+    sixthAnswer = prompt('Guess a numer between 1-10.');}
+  else if (sixthAnswer > 5)
+  {
+    alert('Too high!');
+    sixthAnswer = prompt('Guess a numer between 1-10.');}
+  else if(sixthAnswer === 5)
+  {
+    alert('Correct!');
+    youserScore++;}
+  else
+  {
+    sixthAnswer = prompt('Guess a numer between 1-10.');
+  }
+  i++;
+} while (i<=3 );
+{if (i === 3)
+  alert('Aww Man! It was Five!');}
+
+
+let youserAnswer =[''];
+let seventhAnswer = [''];
+seventhAnswer.push('The Hobbit','Talking with Strangers','Effective Speaking','The Four Agreements','Mans Search for Meaning','What Happened to you?','The Alchemist','The Good Earth','Narrative of the Life of Frederick Douglass','All Gods Children Need Traveling Shoes');
+
+//youserAnswer [j] = prompt('This is the hardest to guess. Can you guess my top five books? Enter your choice from the following:'+ seventhAnswer);
+for(let j = 0;j<=6; j++)
+{
+  youserAnswer [j] = prompt('This is the hardest to guess. Can you guess my top five books? Enter your choice from the following:'+ seventhAnswer);
+
+  if(seventhAnswer.includes (youserAnswer[j]))
+  {
+    youserScore + 1;
+    alert('Correct!');
+    youserAnswer [j] = prompt('This is the hardest to guess. Can you guess my top five books? Enter your choice from the following:'+ seventhAnswer);
+  }
+  else
+  {
+    alert('Try again!');
+    youserAnswer[j] = prompt('This is the hardest to guess. Can you guess my top five books? Enter your choice from the following:'+ seventhAnswer);
+  }
+}
+alert('My top five favorite books are' + seventhAnswer.slice(0,5));
+
 //give final message include username in alert
 
-alert('This has been so much fun! Until next time, Thanks for playing ' + youserName);
+alert('This has been so much fun! Until next time, Thanks for playing ' + youserName + 'Your final score:' + youserScore + ' out of 7');
